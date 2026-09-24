8790a67a094872f3968d12306da6f6cdea91228a
kysonic <soooyc@gmail.com>
2026-09-24 17:21:53 +0300

    chore: initial commit - setup

---
+++ bun/src/server/streaming-ssr.ts
@@ -0,0 +1,25 @@
+import { createReadableStream } from 'stream/web';
+import { renderToReadableStream } from 'react-dom/server';
+import React, { Suspense } from 'react';
+
+interface StreamingSSRConfig {
+    bootstrapScripts?: string[];
+    bootstrapModules?: string[];
+    onShellReady?: () => void;
+    onAllReady?: () => void;
+    onError?: (error: Error) => void;
+    timeout?: number;
+}
+
+
+export class StreamingSSRRendering {
+    constructor(private config: StreamingSSRConfig = {}) {}
+
+    renderToStream(element: React.ReactElement): ReadableStream<Uint8Array> {
+        const stream = renderToReadableStream(element, {
+            bootstrapScripts: this.config.bootstrapScripts || ['/build/client.js'],
+        });
+
+        return stream;
+    }
+}
